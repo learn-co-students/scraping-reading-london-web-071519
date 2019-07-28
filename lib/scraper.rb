3c56d283cc6a786fc9185661a4bf4e0075f18029
eliothowes1090 <ejhowes@gmail.com>
2019-07-28 16:13:21 +0100

    Used Nokogiri to scrap text data from flatiron.com

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
 
+title = doc.css(".headline-26OIBN")
+courses = doc.css(".title-oE5vT4")
+
+courses.each {|course| puts course.text}
 
