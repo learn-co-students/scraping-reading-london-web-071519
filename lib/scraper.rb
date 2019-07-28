67a1822a95a621e0d70d8a78a44fd6aa6bc11766
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-28 16:39:47 +0100

    scraping

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,12 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+Nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(html)
+
+doc.css(".site-header__hero__headline")
+
+doc.css(".site-header__hero__headline").text
 
 
